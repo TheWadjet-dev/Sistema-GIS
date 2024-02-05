5c67b47bc0dee2cb5cce994395947d35ebeedc03
Stiwar Saltos <69290901+TheWadjet-dev@users.noreply.github.com>
2024-02-05 15:43:59 -0500

    Inventario add

---
+++ Sistema GIS/Models/Inventario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sistema_GIS.Models
+{
+    [Table("Inventario")]
+    internal class Inventario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idInventario { get; set; }
+
+        [StringLength(50)]
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public double? Precio { get; set; }
+        public int? Cantidad { get; set; }
+    }
+}
