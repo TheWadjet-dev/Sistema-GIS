ca4945048d918990dc98b9d03df05f8b878cc978
Stiwar Saltos <69290901+TheWadjet-dev@users.noreply.github.com>
2024-02-17 16:18:29 -0500

    video 14

--- Sistema GIS/obj/Debug/net7.0/Sistema GIS.AssemblyInfo.cs
+++ Sistema GIS/obj/Debug/net7.0/Sistema GIS.AssemblyInfo.cs
@@ -15,4 +15,10 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema GIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6553b60dcb38fe6500c34062e27d187b4d3667")]
+[assembly: System.Reflection.AssemblyProductAttribute("Sistema GIS")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Sistema GIS")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
+// Generado por la clase WriteCodeFragment de MSBuild.
 
