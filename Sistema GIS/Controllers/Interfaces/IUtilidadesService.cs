c0815674f37a95befbd65b88c1ff8eb20c43f570
ERICK HUMBERTO BONILLA PERUGACHI <ehbonilla@uce.edu.ec>
2024-02-17 11:25:41 -0500

    hi

---
+++ Sistema GIS/Controllers/Interfaces/IUtilidadesService.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema_GIS.Controllers.Interfaces
+{
+    public interface IUtilidadesService
+    {
+
+        string GenerarClave();
+
+        string ConvertirSha256(String texto);
+
+    }
+}
