08d18ac1c551c98a51d52fd7f5d51578db3a9e53
Bryan <bryananthony763@gmail.com>
2024-02-18 00:15:40 -0500

    Versión Final de Trabajo Usar Esta o las siguientes

--- Sistema GIS/DAL/DBContext/ORMDBContext.cs
+++ Sistema GIS/DAL/DBContext/ORMDBContext.cs
@@ -13,24 +13,23 @@ namespace Sistema_GIS.DAL.DBContext
         {
         }
         public virtual DbSet<Categoria> Categoria { get; set; }
-        public virtual DbSet<Cliente> Clientes { get; set; }
-        public virtual DbSet<Compra> Compra { get; set; }
-        public virtual DbSet<DetalleCompra> DetalleCompra { get; set; }
         public virtual DbSet<DetalleVenta> DetalleVenta { get; set; }
         public virtual DbSet<Menu> Menu { get; set; }
 
-        public virtual DbSet<Permisos> Permisos { get; set; }
-        public virtual DbSet<ProductoTienda> ProductoTienda { get; set; }
         public virtual DbSet<Producto> Productos { get; set; }
-        public virtual DbSet<Proveedor> Proveedores { get; set; }
         public virtual DbSet<Rol> Roles { get; set; }
-        public virtual DbSet<SubMenu> SubMenu { get; set; }
+        public virtual DbSet<RolMenu> RolMenu { get; set; }
 
-        public virtual DbSet<Tienda> Tiendas { get; set; }
+        public virtual DbSet<Negocio> Negocios { get; set; }
 
-        public virtual DbSet<Usuarios> Usuarios { get; set; }
+        public virtual DbSet<NumeroCorrelativo> NumeroCorrelativos { get; set; }
 
         public virtual DbSet<Venta> Ventas { get; set; }
+
+        public virtual DbSet<TipoDocumentoVenta> TipoDocumentoVentas { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+
 
         /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseSqlServer("Data Source=DESKTOP-2PHOOVO;Initial Catalog=testUniversidadBP;Integrated Security=True;Trust Server Certificate=True");*/
